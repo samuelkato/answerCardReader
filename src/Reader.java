9d831a774a8033c742e94e5322462bdf4a3969da
samuelkato <samuelkato@gmail.com>
2014-06-16 19:35:07 -0300

    v2 com coluna extra

--- src/Reader.java
+++ src/Reader.java
@@ -239,14 +239,14 @@ public class Reader  extends JPanel implements ActionListener, PropertyChangeLis
     			default:
     				throw e;
     			}
-    		}finally{    			
+    		}finally{
     			clImg=new ImageProcessing(file);
     			reg=clImg.regionProps();
     			pontosRef=procurar3pontos(clImg);
     		}
 
     		boolean[][] m = clImg.m;
-    		
+    		System.out.println(pontosRef);
     		String qr=lerQr(file, pontosRef, clImg.width);
     		if(qr=="")throw new Exception("qr code null");
     		
@@ -327,7 +327,7 @@ public class Reader  extends JPanel implements ActionListener, PropertyChangeLis
     			//int distx=clock2.centrox-clock1.centrox;
     			double aClock=(double)(clock2.centroy-clock1.centroy)/(double)(clock2.centrox-clock1.centrox);
     			double bClock=(double)clock1.centroy-aClock*(double)clock1.centrox;
-    			double clockDistX=((double)(clock2.centrox-clock1.centrox))/25;
+    			double clockDistX=((double)(clock2.centrox-clock1.centrox))/26;//numero de divisoes entre o clocks
     			for(int j=1;j<25;j++){
     				if((j-1)%6==0)continue;
     				
@@ -373,20 +373,39 @@ public class Reader  extends JPanel implements ActionListener, PropertyChangeLis
     		//procurar areas brancas delimitadas
     		
     		//evitar "furos" nos 3 quadrados delimitantes
-    		clImg.mInv=clImg.erode(clImg.mInv);
+    		//clImg.mInv=clImg.erode(clImg.mInv);
     		
-    		int[][] bwInv=clImg.bwlabel(clImg.mInv);
+    		int[][] bw=clImg.bwlabel(clImg.m);
 //    		clImg.saveFilteredImage("/home/samuelkato/tmp1.bmp",clImg.m);
 //    		clImg.saveFilteredImage("/home/samuelkato/tmp2.bmp",clImg.mInv);
 //    		clImg.saveFilteredImage("/home/samuelkato/tmp3.bmp",bwInv);
     		
     		ConfigImageProcessing config=new ConfigImageProcessing();
-    		config.minArea=700;
-    		config.maxArea=1800;
-    		List<Region> regInv=clImg.filterRegions(clImg.regionProps(bwInv),config);
+    		config.minArea=200;
+    		config.maxArea=400;
+    		List<Region> regOut=clImg.filterRegions(clImg.regionProps(bw),config);
+    		config.minArea=500;
+    		config.maxArea=700;
+    		List<Region> regIn=clImg.filterRegions(clImg.regionProps(bw),config);
+
+    		List<Region> regInv=new Vector<Region>();
     		
-    		if(regInv.size()<3)throw new Exception("Erro: 3 pontos não encontrados");
+    		for(int i=0; i<regIn.size(); i++){
+    			for(int j=0; j<regOut.size(); j++){
+    				int folga=3;
+    				int xout=regOut.get(j).centrox;
+    				int yout=regOut.get(j).centroy;
+    				int xin=regIn.get(i).centrox;
+    				int yin=regIn.get(i).centroy;
+    				if(xout-folga <= xin && xout+folga >= xin && yout-folga <= yin && yout+folga >= yin){
+    					regInv.add(regIn.get(i));
+    				}
+    			}
+    		}
+    		if(regInv.size()<3)throw new Exception("Erro: 3 pontos não encontrados (tamanho)");
+    		
     		regInv = busca3pontos(regInv);
+    		if(regInv==null)throw new Exception("Erro: 3 pontos não encontrados (90)");
     		
     		List<Region> ret=new Vector<Region>();
     		
@@ -542,7 +561,7 @@ public class Reader  extends JPanel implements ActionListener, PropertyChangeLis
     		
     		
     		int cnt=0;
-    		BufferedImage qrImage=file.getSubimage((int)((pontosRef.get(1).centrox+115)*prop), (int)((pontosRef.get(1).centroy-130)*prop), (int)(160*prop), (int)(160*prop));
+    		BufferedImage qrImage=file.getSubimage((int)((pontosRef.get(1).centrox+185)*prop), (int)((pontosRef.get(1).centroy-130)*prop), (int)(160*prop), (int)(160*prop));
     		//clone
     		BufferedImage small=qrImage.getSubimage(0, 0, qrImage.getWidth(), qrImage.getHeight());
     		BufferedImage bwImg=null;
@@ -573,7 +592,7 @@ public class Reader  extends JPanel implements ActionListener, PropertyChangeLis
     			
     			class configThresh extends ConfigImageProcessing{
     				public boolean checkThreshold(int r, int g, int b, int rAvg, int gAvg, int bAvg){
-    					return r + g + b < (rAvg + gAvg + bAvg - 50);
+    					return r + g + b < (rAvg + gAvg + bAvg - 50) && !(r>g+10 && r>b+10 && r>150);
     				}
     			}
     			
